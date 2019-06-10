4919d4617f91ec9758a3ee0ea7c0ba2af5cb7887
Franklin Chieze <chieze.franklin@gmail.com>
2019-06-10 18:42:59 +0100

    set up tabler-react

---
+++ src/components/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import SiteWrapper from '../layouts/SiteWrapper';
+
+import {
+  Page,
+  Avatar,
+  Icon,
+  Grid,
+  Card,
+  Text,
+  Table,
+  Alert,
+  Progress,
+  colors,
+  Dropdown,
+  Button,
+  StampCard,
+  StatsCard,
+  ProgressCard,
+  Badge,
+} from "tabler-react";
+
+function Home() {
+  return (<SiteWrapper>
+    <Page.Content title="PRLint"></Page.Content>
+  </SiteWrapper>);
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import "./c3jscustom.css";
+import './tabler.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
