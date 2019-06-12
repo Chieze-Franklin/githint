648ddaa9ec83221cde1fca2394e53d282bdc7fae
Franklin Chieze <chieze.franklin@gmail.com>
2019-06-13 00:18:30 +0100

    complete data for commit

--- src/components/TreeView.js
+++ src/components/TreeView.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import TreeMenu from 'react-simple-tree-menu';
 
+const keyStyle = {
+  color: 'blue'
+}
+const getValueStyle = (value) => {
+  if (value.startsWith('"')) {
+    return {
+      color: 'brown'
+    }
+  } else if (new RegExp('[0-9]+').test(value)) {
+    return {
+      color: 'teal'
+    }
+  } else if (new RegExp('((true)|(false))').test(value)) {
+    return {
+      color: 'magenta'
+    }
+  } else {
+    return {
+      color: 'black'
+    }
+  }
+}
+
 function TreeView(props) {
+  const locale = ({label, example}) => {
+    return example ?
+      <div>
+        <span style={keyStyle}>{label}</span>:{" "}
+        <span style={getValueStyle(example)}>{example}</span>
+      </div> :
+      <span style={keyStyle}>{label}</span>;
+  };
   const matchSearch = ({key, searchTerm}) => {
     return new RegExp(searchTerm.replace('[', '\\[')).test(key);
   };
@@ -10,8 +41,9 @@ function TreeView(props) {
   };
   return (<TreeMenu
     data={props.data}
-    onClickItem={props.onClickItem || onClickItem}
+    locale={props.locale || locale}
     matchSearch={props.matchSearch || matchSearch}
+    onClickItem={props.onClickItem || onClickItem}
     />);
 }
 
--- src/data/commit.js
+++ src/data/commit.js
@@ -15,14 +15,105 @@ export default [
     example: '"MDY6Q29tbWl0NmRjYjA5YjViNTc4NzVmMzM0ZjYxYWViZWQ2OTVlMmU0MTkzZGI1ZQ=="'
   },
   {
-    key: 'commit.html_url',
-    label: 'html_url',
-    example: '"https://github.com/octocat/Hello-World/commit/6dcb09b5b57875f334f61aebed695e2e4193db5e"',
-  },
-  {
     key: 'commit.comments_url',
     label: 'comments_url',
     example: '"https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e/comments"'
+  },
+  {
+    key: 'commit.author',
+    label: 'author',
+    nodes: [
+      {
+        key: 'commit.author.id',
+        label: 'id',
+        example: '1'
+      },
+      {
+        key: 'commit.author.login',
+        label: 'login',
+        example: '"octocat"'
+      },
+      {
+        key: 'commit.author.node_id',
+        label: 'node_id',
+        example: '"MDQ6VXNlcjE="'
+      },
+      {
+        key: 'commit.author.avatar_url',
+        label: 'avatar_url',
+        example: '"https://github.com/images/error/octocat_happy.gif"'
+      },
+      {
+        key: 'commit.author.gravatar_id',
+        label: 'gravatar_id',
+        example: '""'
+      },
+      {
+        key: 'commit.author.url',
+        label: 'url',
+        example: '"https://api.github.com/users/octocat"'
+      },
+      {
+        key: 'commit.author.events_url',
+        label: 'events_url',
+        example: '"https://api.github.com/users/octocat/events{/privacy}"'
+      },
+      {
+        key: 'commit.author.followers_url',
+        label: 'followers_url',
+        example: '"https://api.github.com/users/octocat/followers"'
+      },
+      {
+        key: 'commit.author.following_url',
+        label: 'following_url',
+        example: '"https://api.github.com/users/octocat/following{/other_user}"'
+      },
+      {
+        key: 'commit.author.gists_url',
+        label: 'gists_url',
+        example: '"https://api.github.com/users/octocat/gists{/gist_id}"'
+      },
+      {
+        key: 'commit.author.html_url',
+        label: 'html_url',
+        example: '"https://github.com/octocat"'
+      },
+      {
+        key: 'commit.author.organizations_url',
+        label: 'organizations_url',
+        example: '"https://api.github.com/users/octocat/orgs"'
+      },
+      {
+        key: 'commit.author.received_events_url',
+        label: 'received_events_url',
+        example: '"https://api.github.com/users/octocat/received_events"'
+      },
+      {
+        key: 'commit.author.repos_url',
+        label: 'repos_url',
+        example: '"https://api.github.com/users/octocat/repos"'
+      },
+      {
+        key: 'commit.author.site_admin',
+        label: 'site_admin',
+        example: 'false'
+      },
+      {
+        key: 'commit.author.starred_url',
+        label: 'starred_url',
+        example: '"https://api.github.com/users/octocat/starred{/owner}{/repo}"'
+      },
+      {
+        key: 'commit.author.subscriptions_url',
+        label: 'subscriptions_url',
+        example: '"https://api.github.com/users/octocat/subscriptions"'
+      },
+      {
+        key: 'commit.author.type',
+        label: 'type',
+        example: '"User"'
+      },
+    ]
   },
   {
     key: 'commit.commit',
@@ -76,6 +167,11 @@ export default [
         ],
       },
       {
+        key: 'commit.commit.comment_count',
+        label: 'comment_count',
+        example: '0'
+      },
+      {
         key: 'commit.commit.message',
         label: 'message',
         example: '"Fix all the bugs"'
@@ -95,11 +191,6 @@ export default [
             example: '"6dcb09b5b57875f334f61aebed695e2e4193db5e"'
           },
         ],
-      },
-      {
-        key: 'commit.commit.comment_count',
-        label: 'comment_count',
-        example: '0'
       },
       {
         key: 'commit.commit.verification',
@@ -128,5 +219,213 @@ export default [
         ],
       },
     ]
+  },
+  {
+    key: 'commit.committer',
+    label: 'committer',
+    nodes: [
+      {
+        key: 'commit.committer.id',
+        label: 'id',
+        example: '1'
+      },
+      {
+        key: 'commit.committer.login',
+        label: 'login',
+        example: '"octocat"'
+      },
+      {
+        key: 'commit.committer.node_id',
+        label: 'node_id',
+        example: '"MDQ6VXNlcjE="'
+      },
+      {
+        key: 'commit.committer.avatar_url',
+        label: 'avatar_url',
+        example: '"https://github.com/images/error/octocat_happy.gif"'
+      },
+      {
+        key: 'commit.committer.gravatar_id',
+        label: 'gravatar_id',
+        example: '""'
+      },
+      {
+        key: 'commit.committer.url',
+        label: 'url',
+        example: '"https://api.github.com/users/octocat"'
+      },
+      {
+        key: 'commit.committer.events_url',
+        label: 'events_url',
+        example: '"https://api.github.com/users/octocat/events{/privacy}"'
+      },
+      {
+        key: 'commit.committer.followers_url',
+        label: 'followers_url',
+        example: '"https://api.github.com/users/octocat/followers"'
+      },
+      {
+        key: 'commit.committer.following_url',
+        label: 'following_url',
+        example: '"https://api.github.com/users/octocat/following{/other_user}"'
+      },
+      {
+        key: 'commit.committer.gists_url',
+        label: 'gists_url',
+        example: '"https://api.github.com/users/octocat/gists{/gist_id}"'
+      },
+      {
+        key: 'commit.committer.html_url',
+        label: 'html_url',
+        example: '"https://github.com/octocat"'
+      },
+      {
+        key: 'commit.committer.organizations_url',
+        label: 'organizations_url',
+        example: '"https://api.github.com/users/octocat/orgs"'
+      },
+      {
+        key: 'commit.committer.received_events_url',
+        label: 'received_events_url',
+        example: '"https://api.github.com/users/octocat/received_events"'
+      },
+      {
+        key: 'commit.committer.repos_url',
+        label: 'repos_url',
+        example: '"https://api.github.com/users/octocat/repos"'
+      },
+      {
+        key: 'commit.committer.site_admin',
+        label: 'site_admin',
+        example: 'false'
+      },
+      {
+        key: 'commit.committer.starred_url',
+        label: 'starred_url',
+        example: '"https://api.github.com/users/octocat/starred{/owner}{/repo}"'
+      },
+      {
+        key: 'commit.committer.subscriptions_url',
+        label: 'subscriptions_url',
+        example: '"https://api.github.com/users/octocat/subscriptions"'
+      },
+      {
+        key: 'commit.committer.type',
+        label: 'type',
+        example: '"User"'
+      },
+    ]
+  },
+  {
+    key: 'commit.files',
+    label: 'files',
+    nodes: [
+      {
+        key: 'commit.files[0]|commit.files.0',
+        label: '0',
+        nodes: [
+          {
+            key: 'commit.files[0].sha|commit.files.0.sha',
+            label: 'sha',
+            example: '"8c1fe41bb646b10782fb602cd1f0aa3dc827c3cb"'
+          },
+          {
+            key: 'commit.files[0].filename|commit.files.0.filename',
+            label: 'filename',
+            example: '"file1.txt"'
+          },{
+            key: 'commit.files[0].additions|commit.files.0.additions',
+            label: 'additions',
+            example: '10'
+          },
+          {
+            key: 'commit.files[0].deletions|commit.files.0.deletions',
+            label: 'deletions',
+            example: '2'
+          },
+          {
+            key: 'commit.files[0].changes|commit.files.0.changes',
+            label: 'changes',
+            example: '12'
+          },
+          {
+            key: 'commit.files[0].status|commit.files.0.status',
+            label: 'status',
+            example: '"modified"'
+          },
+          {
+            key: 'commit.files[0].blob_url|commit.files.0.blob_url',
+            label: 'blob_url',
+            example: '"https://github.com/octocat/Hello-World/blob/7ca483543807a51b6079e54ac4cc392bc29ae284/file1.txt"'
+          },
+          {
+            key: 'commit.files[0].contents_url|commit.files.0.contents_url',
+            label: 'contents_url',
+            example: '"https://api.github.com/repos/octocat/Hello-World/contents/file1.txt?ref=7ca483543807a51b6079e54ac4cc392bc29ae284"'
+          },
+          {
+            key: 'commit.files[0].raw_url|commit.files.0.raw_url',
+            label: 'raw_url',
+            example: '"https://github.com/octocat/Hello-World/raw/7ca483543807a51b6079e54ac4cc392bc29ae284/file1.txt"'
+          },
+          {
+            key: 'commit.files[0].patch|commit.files.0.patch',
+            label: 'patch',
+            example: '"@@ -29,7 +29,7 @@\n....."'
+          },
+        ]
+      },
+    ]
+  },
+  {
+    key: 'commit.parents',
+    label: 'parents',
+    nodes: [
+      {
+        key: 'commit.parents[0]|commit.parents.0',
+        label: '0',
+        nodes: [
+          {
+            key: 'commit.parents[0].sha|commit.parents.0.sha',
+            label: 'sha',
+            example: '"6dcb09b5b57875f334f61aebed695e2e4193db5e"'
+          },
+          {
+            key: 'commit.parents[0].html_url|commit.parents.0.html_url',
+            label: 'html_url',
+            example: '"https://github.com/octocat/Hello-World/commit/6dcb09b5b57875f334f61aebed695e2e4193db5e"'
+          },
+          {
+            key: 'commit.parents[0].url|commit.parents.0.url',
+            label: 'url',
+            example: '"https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e"'
+          },
+        ]
+      },
+    ]
+  },
+  {
+    key: 'commit.stats',
+    label: 'stats',
+    nodes: [
+      {
+        key: 'commit.stats.additions',
+        label: 'additions',
+        example: '104'
+      },{
+        key: 'commit.stats.deletions',
+        label: 'deletions',
+        example: '4'
+      },{
+        key: 'commit.stats.total',
+        label: 'total',
+        example: '108'
+      },
+    ]
+  },
+  {
+    key: 'commit.html_url',
+    label: 'html_url',
+    example: '"https://github.com/octocat/Hello-World/commit/6dcb09b5b57875f334f61aebed695e2e4193db5e"',
   },
 ];
