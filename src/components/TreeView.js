2dde9708824eee8273005f0c1062397ef39effa6
Franklin Chieze <chieze.franklin@gmail.com>
2019-07-23 19:24:31 +0100

    fix: fix tree view
    - fix tree view
    
    [finishes #007]

--- src/components/TreeView.js
+++ src/components/TreeView.js
@@ -1,319 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 
+// https://js.devexpress.com/Documentation/ApiReference/UI_Widgets/dxTreeView/
 import Tree from 'devextreme-react/tree-view';
 
-const products = [{
-  id: '1',
-  text: 'Stores',
-  expanded: true,
-  items: [{
-    id: '1_1',
-    text: 'Super Mart of the West',
-    expanded: true,
-    items: [{
-      id: '1_1_1',
-      text: 'Video Players',
-      items: [{
-        id: '1_1_1_1',
-        text: 'HD Video Player',
-        price: 220
-      }, {
-        id: '1_1_1_2',
-        text: 'SuperHD Video Player',
-        price: 270
-      }]
-    }, {
-      id: '1_1_2',
-      text: 'Televisions',
-      items: [{
-        id: '1_1_2_1',
-        text: 'SuperLCD 42',
-        price: 1200
-      }, {
-        id: '1_1_2_2',
-        text: 'SuperLED 42',
-        price: 1450
-      }, {
-        id: '1_1_2_3',
-        text: 'SuperLED 50',
-        price: 1600
-      }, {
-        id: '1_1_2_4',
-        text: 'SuperLCD 55',
-        price: 1350
-      }, {
-        id: '1_1_2_5',
-        text: 'SuperLCD 70',
-        price: 4000
-      }]
-    }, {
-      id: '1_1_3',
-      text: 'Monitors',
-      items: [{
-        id: '1_1_3_1',
-        text: '19"',
-        items: [{
-          id: '1_1_3_1_1',
-          text: 'DesktopLCD 19',
-          price: 160
-        }]
-      }, {
-        id: '1_1_3_2',
-        text: '21"',
-        items: [{
-          id: '1_1_3_2_1',
-          text: 'DesktopLCD 21',
-          price: 170
-        }, {
-          id: '1_1_3_2_2',
-          text: 'DesktopLED 21',
-          price: 175
-        }]
-      }]
-    }, {
-      id: '1_1_4',
-      text: 'Projectors',
-      items: [{
-        id: '1_1_4_1',
-        text: 'Projector Plus',
-        price: 550
-      }, {
-        id: '1_1_4_2',
-        text: 'Projector PlusHD',
-        price: 750
-      }]
-    }]
-
-  }, {
-    id: '1_2',
-    text: 'Braeburn',
-    items: [{
-      id: '1_2_1',
-      text: 'Video Players',
-      items: [{
-        id: '1_2_1_1',
-        text: 'HD Video Player',
-        price: 240
-      }, {
-        id: '1_2_1_2',
-        text: 'SuperHD Video Player',
-        price: 300
-      }]
-    }, {
-      id: '1_2_2',
-      text: 'Televisions',
-      items: [{
-        id: '1_2_2_1',
-        text: 'SuperPlasma 50',
-        price: 1800
-      }, {
-        id: '1_2_2_2',
-        text: 'SuperPlasma 65',
-        price: 3500
-      }]
-    }, {
-      id: '1_2_3',
-      text: 'Monitors',
-      items: [{
-        id: '1_2_3_1',
-        text: '19"',
-        items: [{
-          id: '1_2_3_1_1',
-          text: 'DesktopLCD 19',
-          price: 170
-        }]
-      }, {
-        id: '1_2_3_2',
-        text: '21"',
-        items: [{
-          id: '1_2_3_2_1',
-          text: 'DesktopLCD 21',
-          price: 180
-        }, {
-          id: '1_2_3_2_2',
-          text: 'DesktopLED 21',
-          price: 190
-        }]
-      }]
-    }]
-
-  }, {
-    id: '1_3',
-    text: 'E-Mart',
-    items: [{
-      id: '1_3_1',
-      text: 'Video Players',
-      items: [{
-        id: '1_3_1_1',
-        text: 'HD Video Player',
-        price: 220
-      }, {
-        id: '1_3_1_2',
-        text: 'SuperHD Video Player',
-        price: 275
-      }]
-    }, {
-      id: '1_3_3',
-      text: 'Monitors',
-      items: [{
-        id: '1_3_3_1',
-        text: '19"',
-        items: [{
-          id: '1_3_3_1_1',
-          text: 'DesktopLCD 19',
-          price: 165
-        }]
-      }, {
-        id: '1_3_3_2',
-        text: '21"',
-        items: [{
-          id: '1_3_3_2_1',
-          text: 'DesktopLCD 21',
-          price: 175
-        }]
-      }]
-    }]
-  }, {
-    id: '1_4',
-    text: 'Walters',
-    items: [{
-      id: '1_4_1',
-      text: 'Video Players',
-      items: [{
-        id: '1_4_1_1',
-        text: 'HD Video Player',
-        price: 210
-      }, {
-        id: '1_4_1_2',
-        text: 'SuperHD Video Player',
-        price: 250
-      }]
-    }, {
-      id: '1_4_2',
-      text: 'Televisions',
-      items: [{
-        id: '1_4_2_1',
-        text: 'SuperLCD 42',
-        price: 1100
-      }, {
-        id: '1_4_2_2',
-        text: 'SuperLED 42',
-        price: 1400
-      }, {
-        id: '1_4_2_3',
-        text: 'SuperLED 50',
-        price: 1500
-      }, {
-        id: '1_4_2_4',
-        text: 'SuperLCD 55',
-        price: 1300
-      }, {
-        id: '1_4_2_5',
-        text: 'SuperLCD 70',
-        price: 4000
-      }, {
-        id: '1_4_2_6',
-        text: 'SuperPlasma 50',
-        price: 1700
-      }]
-    }, {
-      id: '1_4_3',
-      text: 'Monitors',
-      items: [{
-        id: '1_4_3_1',
-        text: '19"',
-        items: [{
-          id: '1_4_3_1_1',
-          text: 'DesktopLCD 19',
-          price: 160
-        }]
-      }, {
-        id: '1_4_3_2',
-        text: '21"',
-        items: [{
-          id: '1_4_3_2_1',
-          text: 'DesktopLCD 21',
-          price: 170
-        }, {
-          id: '1_4_3_2_2',
-          text: 'DesktopLED 21',
-          price: 180
-        }]
-      }]
-    }, {
-      id: '1_4_4',
-      text: 'Projectors',
-      items: [{
-        id: '1_4_4_1',
-        text: 'Projector Plus',
-        price: 550
-      }, {
-        id: '1_4_4_2',
-        text: 'Projector PlusHD',
-        price: 750
-      }]
-    }]
-
-  }]
-}];
-
-const keyStyle = {
-  color: 'blue'
-}
+const keyStyle = 'color:blue'
 const getValueStyle = (value) => {
   value = value.toString();
   if (value.startsWith('"')) {
-    return {
-      color: 'brown'
-    }
+    return 'color:brown';
   } else if (new RegExp('[0-9]+').test(value)) {
-    return {
-      color: 'teal'
-    }
+    return 'color:teal';
   } else if (new RegExp('((true)|(false))').test(value)) {
-    return {
-      color: 'magenta'
-    }
+    return 'color:magenta';
   } else {
-    return {
-      color: 'black'
-    }
+    return 'color:black';
   }
 }
 
 function TreeView(props) {
-  const [searchMode, setSearchMode] = useState('contains');
-  const locale = ({label, example}) => {
-    return typeof example !== 'undefined' ?
-      <div>
-        <span style={keyStyle}>{label}</span>:{" "}
-        <span style={getValueStyle(example)}>{example}</span>
-      </div> :
-      <span style={keyStyle}>{label}</span>;
+  const display = ({label, example}) => {
+    return typeof example !== 'undefined' ? `${label}: ${example}` : `${label}`;
   };
-  const matchSearch = ({key, searchTerm}) => {
-    return new RegExp(searchTerm.replace('[', '\\[')).test(key);
+  const template = (itemData, itemIndex, element) => {
+    // element.append(
+    //   itemData.label
+    // )
+    var d = document.createElement("div");
+    const {label, example} = itemData;
+    if (typeof example !== 'undefined') {
+      d.innerHTML = `<span style=${keyStyle}>${label}</span>: ` +
+        `<span style=${getValueStyle(example)}>${example}</span>`;
+    } else {
+      d.innerHTML = `<span style=${keyStyle}>${label}</span>`;
+    }
+    return d;
   };
-  const onClickItem = (props) => {
-    // console.log(props)
-  };
-  // return (<TreeMenu
-  //   data={props.data}
-  //   locale={props.locale || locale}
-  //   matchSearch={props.matchSearch || matchSearch}
-  //   onClickItem={props.onClickItem || onClickItem}
-  //   />);
+  
   return (
     <React.Fragment>
       <Tree
         id={'treeview'}
-        items={products}
-        width={500}
-        searchMode={searchMode}
+        displayExpr={display}
+        expandAllEnabled={true}
+        items={props.data}
+        itemsExpr={'nodes'}
+        itemTemplate={template}
+        keyExpr={'key'}
+        noDataText={'No field found matching the search term.'}
         searchEnabled={true}
+        searchExpr={['key']}
+        searchMode={'contains'}
+        width={'auto'}
       />
     </React.Fragment>
   );
